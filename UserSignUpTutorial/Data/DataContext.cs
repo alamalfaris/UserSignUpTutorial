1c2aba395a30166bd24cccdfb15240a9d28dfc72
Alam Alfaris <alamalfaris97@gmail.com>
2023-04-09 10:16:02 +0700

    Add project files.

---
+++ UserSignUpTutorial/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserSignUpTutorial.Models;
+
+namespace UserSignUpTutorial.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseSqlServer("Server=ALAMALFARIS;Database=usersignupDB;Trusted_Connection=true");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
